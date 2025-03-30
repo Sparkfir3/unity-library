bc2aab00d918f1145113345aa9ea5ba131de454a
Sparkfire <48664233+Sparkfir3@users.noreply.github.com>
2025-03-30 01:38:23 -0500

    WeightedChance drawer support for different value types

--- Assets/Library/Utility/Editor/WeightedChanceEntryDrawer.cs
+++ Assets/Library/Utility/Editor/WeightedChanceEntryDrawer.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 
 namespace Sparkfire.Utility.Editor
@@ -12,10 +13,51 @@ namespace Sparkfire.Utility.Editor
             VisualTreeAsset treeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Library/Utility/Editor/WeightedChanceEntry.uxml");
             root.Add(treeAsset.Instantiate());
 
-            root.Q<FloatField>("Percent").SetEnabled(false);
-            root.Q<FloatField>("Percent").formatString = "P2";
+            InitializePercentField(root);
+            InitializeValueField(property, root);
 
             return root;
+        }
+
+        private void InitializePercentField(in VisualElement root)
+        {
+            FloatField percentField = root.Q<FloatField>("Percent");
+            percentField.SetEnabled(false);
+            percentField.formatString = "P2";
+        }
+
+        private void InitializeValueField(in SerializedProperty property, in VisualElement root)
+        {
+            FloatField baseValueField = root.Q<FloatField>("Value");
+            SerializedPropertyType propertyType = property.FindPropertyRelative("value").propertyType;
+            BindableElement newValueField = propertyType switch
+            {
+                SerializedPropertyType.Integer => new IntegerField(),
+                SerializedPropertyType.Boolean => new Toggle(),
+                SerializedPropertyType.Float => new FloatField(),
+                SerializedPropertyType.String => new TextField(),
+                SerializedPropertyType.Color => new ColorField(),
+                SerializedPropertyType.ObjectReference => new ObjectField(),
+                SerializedPropertyType.LayerMask => new LayerMaskField(),
+                SerializedPropertyType.Enum => new EnumField(),
+                SerializedPropertyType.Vector2 => new Vector2Field(),
+                SerializedPropertyType.Vector3 => new Vector3Field(),
+                SerializedPropertyType.Vector4 => new Vector4Field(),
+                SerializedPropertyType.AnimationCurve => new CurveField(),
+                SerializedPropertyType.Quaternion => new Vector3Field(),
+                _ => baseValueField
+            };
+            newValueField.bindingPath = baseValueField.bindingPath;
+            newValueField.name = baseValueField.name;
+            newValueField.style.flexShrink = 1;
+            newValueField.style.flexGrow = 1;
+            newValueField.style.width = 100;
+            newValueField.style.marginLeft = baseValueField.style.marginLeft.value.value;
+            newValueField.style.marginRight = baseValueField.style.marginRight.value.value;
+
+            VisualElement container = root.Q<VisualElement>("Container");
+            container.Remove(baseValueField);
+            container.Add(newValueField);
         }
     }
 }
